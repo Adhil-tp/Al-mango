f6fdd69a00c51b1de5f2985d06a65b95354ab4ba
Ansab11 <ansabvk122@gmail.com>
2024-09-20 04:47:35 -0700

    header is finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import Header from "./components/Navbar"
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="text-3xl font-bold underline">hello world</div>
+    <Header />
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex justify-between  p-4 bg-black text-white w-full h-60 fixed bottom-0 left-0 shadow-md">
+      <h1 className="font-semibold text-3xl ml-12 mt-8 ">Al-Mango</h1>
+    </div>
+  )
+}
+
+export default Footer
