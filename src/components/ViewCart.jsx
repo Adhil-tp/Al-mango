f4f0068fcacb553217e47be49266afa83939c62a
deekshithm0 <deekshithm0@gmail.com>
2024-09-21 11:49:44 +0530

    menu structure

--- src/components/ViewCart.jsx
+++ src/components/ViewCart.jsx
@@ -5,10 +5,10 @@ const ViewCart = () => {
   return (
     <div className='bg-orange-200  flex flex-row w-screen items-center justify-between p-4 '>
       <div className='flex flex-row gap-5 items-center justify-center'>
-        <img src={ProductImg} alt="productImage" className='w-10 h-8 rounded' />
+        <img src={ProductImg} alt="productImage" className='w-8 h-8 rounded sm:w-10 sm:h-10' />
         <div>
-          <h6>Product Name</h6>
-          <p className='text-xs'>2 items</p>
+          <h6 className='text-sm sm:text-base'>Product Name</h6>
+          <p className='text-xs sm:text-sm'>2 items</p>
         </div>
       </div>
       <div className='flex w-32 h-10 items-center justify-center'>
