4ea40e04ad08584937bda6401b68ad25657a7add
Ansab11 <ansabvk122@gmail.com>
2024-09-27 06:39:15 -0700

    fixed conflicts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,15 @@ import BgImage from '../assets/backgound/background.jpg'
 
 const Footer = () => {
   return (
-    <div  className="flex justify-between items-center p-4 text-white w-full h-30 fixed bottom-0 left-0 shadow-md">
-    <div style={{ backgroundImage: `url(${BgImage})` }}  className="absolute inset-0 bg-cover bg-center z-0">
+    <div  className="relative flex justify-between items-center p-4 text-white w-full h-30 mt-auto">
+    <div style={{ backgroundImage: `url(${BgImage}) `}}  className="absolute inset-0 bg-cover bg-center z-0">
         {/* Tint Overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-60 z-0"></div>
       </div>
       <div  className=" z-10 mb-36 font-semibold sm:text-3xl md:text-3xl lg:text-4xl">
       <h1> Al-Mango </h1>
       </div>
-      <div className='absolute top-4 right-4 p-2 shadow-md text-right font-semibold'>
+      <div className='absolute top-4 right-6 p-2 shadow-md text-right font-semibold'>
         <h3 className="sm:text-1xl mr-4 ">Contact Us:</h3>
         <p className="text-sm sm:text-1xl">+974 123-456-78</p>
         <p className="text-sm sm:text-1xl">+974 123-456-78</p>
@@ -31,4 +31,4 @@ const Footer = () => {
   );
 }
 
-export default Footer;
+export default Footer;
