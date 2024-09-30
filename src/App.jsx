f2dccad3976a70dfa25d54914630f94f08a315ff
deekshithm0 <deekshithm0@gmail.com>
2024-09-30 11:54:48 +0530

    updations with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react"
 import './App.css'
 import Menu from "./pages/Menu"
-import { CartProvider } from "./context/CartContext"
+// import { CartProvider } from "./context/CartContext"
 
 function App() {
   return (
-    <div className="App">
-      <CartProvider>
-        <Menu/>
-      </CartProvider>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />}  />
+          <Route path="menu" element={<Menu />} />
+          <Route path="product" element={<ProductsDetails />} />
+          {/* <Route path="cart" element={<Cart />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { CartContext } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CartContext>
+      <App />
+    </CartContext>
   </StrictMode>,
 )
